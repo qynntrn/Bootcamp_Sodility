375c8bec507a3f72686a46bae64eca48d2affe4b
qynntrn <tcquyen.work@gmail.com>
2024-11-30 22:16:20 +0700

    add payable modifier

--- contracts/SampleContract.sol
+++ contracts/SampleContract.sol
@@ -5,7 +5,14 @@ pragma solidity >=0.8.2 <0.9.0;
 contract SampleContract {
     string public myS = "Hello World";
 
-    function setString(string memory _s) public {
-        myS = _s;
+    // payable modifier allow smart contract receive eth
+    function setString(string memory _s) public payable   {
+        // only save 1 eth in the contract and update msg
+        if (msg.value == 1 ether){
+            myS = _s;
+        } else { // else it will pay the money again to the address of user
+            payable(msg.sender).transfer(msg.value);
+        }
+       
     }
 }
